c494cb1edf6fbad4d796b8bf350d64c826be654c
khoapham98 <phamanhkhoa9803@gmail.com>
2025-08-05 15:33:40 +0700

    feat: using mutex lock for UART transmit

--- main.c
+++ main.c
@@ -7,22 +7,28 @@
 #include "temp_sensor.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 TaskHandle_t task1;
 TaskHandle_t task2; 
+SemaphoreHandle_t uart_lock; 
 
-extern uint8_t LED;
-extern uint8_t LED_stt;
-extern const char* color_name[];
-extern const char* stt_name[];
+extern int LED; 
+extern int LED_stt;
 
 void task2_read_MCU_temp()
 {
+	TickType_t last_time = xTaskGetTickCount();
 	while (1)
 	{
-		float temp = get_mcu_temp();
-		UART_send_string("STM32 temperature: %.2f\n", temp);
-		vTaskDelay(1000);
+		if (xSemaphoreTake(uart_lock, 500) == pdTRUE)
+		{
+			float temp = get_mcu_temp(); 
+			UART_send_string("STM32 temperature: %.2f\n", temp); 
+			xSemaphoreGive(uart_lock); 
+		}
+		/* vTaskDelay(1000); */
+		vTaskDelayUntil(&last_time, 1000);
 	}
 }
 
@@ -30,12 +36,11 @@ void task1_LED_Ctrl_via_UART()
 {
 	while (1) 
 	{
-		if (LED != 4) 
+		if (LED != -1)
 		{
-			UART_send_string("%s led is %s\n", color_name[LED], stt_name[LED_stt]);
-			LED_Ctrl(LED, LED_stt);
+			LED_Ctrl(LED, LED_stt); 
 		}
-		vTaskDelay(1000);
+		vTaskDelay(500);
 	}
 }
 
@@ -46,7 +51,8 @@ int main()
 	ADC_Init();
 	LED_Init();
 	xTaskCreate(task1_LED_Ctrl_via_UART, "task 1", 512, NULL, 0, &task1);	
-	xTaskCreate(task2_read_MCU_temp, "task 2", 128, NULL, 0, &task2);
+	xTaskCreate(task2_read_MCU_temp, "task 2", 512, NULL, 0, &task2);
+	uart_lock = xSemaphoreCreateMutex();
 	vTaskStartScheduler();	
 	
 	while (1)
